d20d02be484acc8e09bbf2865ae2dc523374e6d4
NguyenDucHy2302 <nguyenduchy2302@gmail.com>
2025-02-13 10:07:50 +0700

    UserManagement add  13/2/2025

--- src/modules/users/routes.tsx
+++ src/modules/users/routes.tsx
@@ -1,12 +1,20 @@
 import { RouteObject } from "react-router-dom";
 import PrivateRoute from "../../routes/PrivateRoute";
 import { Role } from "../../constants/roles";
+import UserManagement from "./pages/UserManagement";
 
-
-export const userRoutes: RouteObject[] = [
-  // {
-  //   path: "/users",
-  //   element: <PrivateRoute allowedRoles={[Role.ADMIN]} />,
-  //   children: [{ path: "", element: <UserManagement /> }],
-  // },
+export const userRoutes = [
+  {
+    path: "/users",
+    element: <UserManagement />, // REMOVE PrivateRoute TEMPORARILY
+  },
 ];
+
+
+//export const userRoutes: RouteObject[] = [
+ // {
+   //  path: "/users",
+     //element: <PrivateRoute allowedRoles={[Role.ADMIN]} />,
+     //children: [{ path: "", element: <UserManagement /> }],
+   //},
+//];
