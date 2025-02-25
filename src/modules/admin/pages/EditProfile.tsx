c0993c736619d0bc1f92597179bd87a7403f1ec3
nganthuy0111 <nttngan0111@gmail.com>
2025-02-25 15:11:54 +0700

    change admin layout

--- src/modules/admin/pages/EditProfile.tsx
+++ src/modules/admin/pages/EditProfile.tsx
@@ -1,7 +1,41 @@
 
 import "./EditProfile.css";
 import Layout from "../../../shared/layouts/Layout";
-import { Input } from "@mui/material";
+import React from 'react';
+
+function EditProfile() {
+  return (
+    <div>
+      <Layout>
+        <div style={{ display: "flex" }}>
+          <div className="profile-edit">
+            <div className="edit-profile-left">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvdt3vH3FBrtBEyM7nPk-ZRYRGXthCIr0SvQ&s"
+                alt=""
+              />
+            </div>
+            <div className="edit-profile-right">
+              <div className="input-info">
+                <input type="text" placeholder="Full name" />
+                <input type="email" placeholder="Email" />
+                <input type="tel" placeholder="Phone" />
+                <input type="text" placeholder="Address" />
+                <input type="password" placeholder="Password" />
+                <input type="password" placeholder="Confirm Password" />
+                <input type="text" placeholder="Department" />
+                <input type="text" placeholder="Describe yourself" />
+                <button className="btn-cancel">Cancel</button>
+                <button className="btn-save">Save</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
+}
+
 function EditProfile() {
   return (
     <div>
