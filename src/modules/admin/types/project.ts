b8e320c8f919cd6541e9d850967cc61ea9d9bcaa
dduongCongSon <sonduongcong8@gmail.com>
2025-02-26 21:24:51 +0700

    fix: data in projectmanager page accored by api return

--- src/modules/admin/types/project.ts
+++ src/modules/admin/types/project.ts
@@ -1,7 +1,13 @@
 export interface Project {
     id: string;
-    name: string;
-    budget: number;
-    startDate: string;
-    endDate: string;
-}
+    project_name: string;
+    project_code: string;
+    project_department: string;
+    project_description: string;
+    project_start_date: string;
+    project_end_date: string;
+    project_members: {
+      user_id: string;
+      project_role: string;
+    }[];
+  }
